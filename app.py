de2fbea16774deace50d0cec3b94a3409e57f5bd
Zhao Chen <qt316770683@icloud.com>
2020-08-09 01:09:15 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import pymysql
+
+app=Flask(__name__)
+app.config['SECRETE_KEY']='SeCreteEKey'
+#app.config['UPLOAD_FOLDER'] = 'images'
+ENV='prod'
+
+if ENV=='dev':
+    app.debug=True
+    app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:qt123123...@localhost/autocomplete'
+else:
+    app.debug=False
+    app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://bad31ca552b4e5:24402813@us-cdbr-east-02.cleardb.com/heroku_5968cd3c50ec929'
+
+app.config['SQLALCHMY_TRACK_MODIFICATIONS']=False
+
+db=SQLAlchemy(app)
+from routes import *
+
+if __name__=='__main__':
+    app.run()
---
+++ routes.py
@@ -0,0 +1,35 @@
+from app import app, db
+from flask import render_template,request,url_for, Response, jsonify, json
+from wtforms import StringField, TextField, Form
+from wtforms.validators import DataRequired, Length
+
+class SearchForm(Form):
+    output=StringField('Output', validators=[DataRequired(),Length(max=250)],render_kw={"placeholder": "input"})
+
+class Output(db.Model):
+    __tablename__ = 'output'
+
+    starting_phrase = db.Column(db.String(250), primary_key=True)
+    following_word = db.Column(db.String(250))
+    count = db.Column(db.Integer)
+
+    def as_dict(self):
+        return {'output': self.starting_phrase+' '+self.following_word}
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form=SearchForm(request.form)
+    return render_template('index.html', form=form)
+
+@app.route('/output')
+def outputdic():
+    res = Output.query.order_by(Output.count.desc())
+    list_output = [r.as_dict() for r in res]
+    return jsonify(list_output)
+
+@app.route('/process', methods=['POST'])
+def process():
+    output = request.form['output']
+    if output:
+        return jsonify({'output':output})
+    return jsonify({'error': 'missing data..'})
